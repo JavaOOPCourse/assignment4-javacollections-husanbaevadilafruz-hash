df43f76c116805a275d1890a48e3c3fca976112d
Diliafruz <husanbaevadilafruz@gmail.com>
2026-02-24 22:01:52 +0600

    Update Issue.java

--- src/model/Issue.java
+++ src/model/Issue.java
@@ -5,7 +5,33 @@ public class Issue implements Comparable<Issue> {
     // TODO: declare fields
     private String description;
     private int urgencyLevel; // 1 = most urgent
+package model;
 
+public class Issue implements  Comparable<Issue> {
+    private String description;
+    private int UrgencyLevel;
+
+    public Issue(String description, int UrgencyLevel) {
+        this.description = description;
+        this.UrgencyLevel = UrgencyLevel;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getUrgencyLevel() {
+        return UrgencyLevel;
+    }
+
+    public int compareTo (Issue other) {
+        return Integer.compare(this.UrgencyLevel, other.UrgencyLevel);
+    }
+
+    public String toString() {
+        return "[priority :"+UrgencyLevel+"]" +description;
+    }
+}
     // TODO: create constructor
 
     // TODO: implement compareTo()
